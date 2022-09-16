a538c4d1a612b0c77c1d86f98e2d3693f8c6ccff
Danish <danishmaulahasbi@gmail.com>
2022-09-16 14:33:38 +0700

    Membuat Autentikasi dengan Email & Password dan Verifikasi

--- lib/app/controllers/auth_controller.dart
+++ lib/app/controllers/auth_controller.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:latihan_firebase1/app/routes/app_pages.dart';
 
@@ -9,33 +10,103 @@ class AuthController extends GetxController {
 
   void signup(String email, String password) async {
     try {
-      await FirebaseAuth.instance
+      UserCredential myUser = await FirebaseAuth.instance
           .createUserWithEmailAndPassword(email: email, password: password);
-          Get.toNamed(Routes.HOME);
+      //send verification email to exsisiting email
+      await myUser.user!.sendEmailVerification();
+      Get.defaultDialog(
+        title: 'Verifikasi Email',
+        middleText: 'Kami telah mengirimkan link verifikasi ke email ${email}',
+        onConfirm: () {
+          Get.back();
+          Get.back();
+        },
+        textConfirm: 'OK',
+        confirmTextColor: Colors.white,
+      );
     } on FirebaseAuthException catch (e) {
       if (e.code == 'weak-password') {
         print('The password provided is too weak.');
-      } else if (e.code == 'wrong-password') {
+        Get.defaultDialog(
+        title: 'Terjadi kesalahan',
+        middleText: 'Password sangat lemah',
+        onConfirm: () {
+          Get.back();
+          Get.back();
+        },
+        textConfirm: 'OK',
+        confirmTextColor: Colors.white,
+      );
+      } else if (e.code == 'email-already-in-use') {
         print('the account already exists for that email.');
+        Get.defaultDialog(
+        title: 'Terjadi Kesalahan',
+        middleText: 'Email sudah terdaftar',
+        onConfirm: () {
+          Get.back();
+          Get.back();
+        },
+        textConfirm: 'OK',
+        confirmTextColor: Colors.white,
+      );
+        
       }
     }
   }
 
   void login(String email, String password) async {
     try {
-      await FirebaseAuth.instance
+      UserCredential myUser = await FirebaseAuth.instance
           .signInWithEmailAndPassword(email: email, password: password);
-      Get.toNamed(Routes.HOME);
+      if (myUser.user!.emailVerified == true) {
+        Get.offAllNamed(Routes.HOME);
+      } else {
+        Get.defaultDialog(
+          title: 'Email belum terverifikasi',
+          middleText: 
+          'Anda memerlukan verifikasi email untuk masuk, apakah anda ingin mengirim ulang verifikasi email?',
+          textConfirm: 'Kirim Ulang',
+          confirmTextColor: Colors.white,
+          onConfirm: () async {
+            await myUser.user!.sendEmailVerification();
+            Get.back();
+          },
+          textCancel: 'Batal',
+          onCancel: () {
+            Get.back();
+          },
+        );
+      }
     } on FirebaseAuthException catch (e) {
       if (e.code == 'user-not-found') {
         print('No user found for that email.');
+        Get.defaultDialog(
+          title: 'Terjadi Kesalahan',
+          middleText: 'User tidak ditemukan',
+          textConfirm: 'OK',
+          confirmTextColor: Colors.white,
+          onConfirm: () {
+            Get.back(); 
+          },
+        );
       } else if (e.code == 'wrong-password') {
         print('Wrong password provided for that user.');
+        Get.defaultDialog(
+          title: 'Terjadi kesalahan',
+          middleText: 'Password salah',
+          textConfirm: 'OK',
+          confirmTextColor: Colors.white,
+          onConfirm: () {
+            Get.back();
+          },
+          
+        );
       }
     }
   }
 
   void logout() async {
     await FirebaseAuth.instance.signOut();
+    Get.offAllNamed(Routes.LOGIN);
   }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -2,15 +2,21 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
+import '../../../controllers/auth_controller.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
+  final authC = Get.find<AuthController>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('HomeView'),
         centerTitle: true,
+        actions: [
+          IconButton(onPressed: () => authC.logout(), icon: Icon(Icons.logout))
+        ],
       ),
       body: Center(
         child: Text(
